cd867f50412f820f08ab7d303ed7629d0563fbcc
stefan-tkii <stefan.kjoropanovski@gmail.com>
2021-01-11 06:58:17 -0800

    Initial Commit

---
+++ UrbanHeroApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UrbanHeroApplication
+//
+//  Created by Stefan Kjoropanovski on 1/11/21.
+//  Copyright © 2021 Stefan Kjoropanovski-Resen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
